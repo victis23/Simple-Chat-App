4ea5563ebe33ca429c7219f34e3990dff59029a1
Michael Wells <wow@duhmarket.com>
2019-11-16 17:42:09 -0500

    Initial Commit

---
+++ BasicChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicChatApp
+//
+//  Created by Scott Leonard on 11/16/19.
+//  Copyright © 2019 DuhMarket. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
